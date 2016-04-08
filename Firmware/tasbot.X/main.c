a3b522d1374d7059a56af4871df26bc6da645724
Claudio Mutti <cmutti@student.42.fr>
2016-04-08 09:14:56 +0200

    tp4 started

--- Firmware/tasbot.X/main.c
+++ Firmware/tasbot.X/main.c
@@ -53,7 +53,7 @@ void    tp2(void)
             was_press = 0;
     }
 }
-
+/*
 void    __ISR(7, IPL1AUTO)  button_change_timer(void)
 {
             if (T2CONbits.TCKPS == 2)
@@ -68,6 +68,7 @@ void    __ISR (8, IPL2AUTO) timer()
     IFS0bits.T2IF = 0;
     LATFbits.LATF1 = !LATFbits.LATF1;
 }
+*/
 
 void    tp3(void)
 {
@@ -96,8 +97,72 @@ void    tp3(void)
     }
 }
 
+void    __ISR(7, IPL1AUTO)  button_change_timer(void)
+{
+        LATFbits.LATF1 = !LATFbits.LATF1;
+            if (T2CONbits.TCKPS == 2)
+                T2CONbits.TCKPS = 6;
+            else
+                T2CONbits.TCKPS--;
+            IFS0bits.INT1IF = 0;
+}
+
+void    __ISR (6, IPL1AUTO) timerOC()
+{
+    IFS0bits.OC1IF = 0;
+    LATFbits.LATF1 = !LATFbits.LATF1;
+    TMR3SET = 0;
+}
+
+void    tp4(void)
+{
+    
+    LATFbits.LATF1 = 1;
+    TRISFbits.TRISF1 = 0;
+    T2CONbits.TCKPS = 6;
+    T2CONbits.ON = 0;
+    PR2 = 15625;
+    
+//    asm volatile("ei");     //enable interrupt
+    __builtin_enable_interrupts(); // same as above
+    INTCONbits.MVEC = 1;    // enables multi-vector mode
+
+    INTCONbits.INT1EP = 0;  // detect le rising edge
+    IEC0bits.INT1IE = 1;    // enables external interrupt from BUT
+    IPC1bits.INT1IP = 1;    // Priority level same as sub priority
+    IPC1bits.INT1IS = 1;    // SubPriority level
+
+    IEC0bits.T2IE = 1;    // enables external interrupt from BUT
+    IPC2bits.T2IP = 2;    // Priority level same as sub priority
+    IPC2bits.T2IS = 2;    // SubPriority level
+
+            //      PWM PART        //
+    PR3 = 15625;
+    OC1R = 10;
+    OC1RS = 0;
+
+    OC1CONbits.ON = 1;      // enable output compare
+    OC1CONbits.SIDL = 0;    // continue operations in idle mode
+    OC1CONbits.OC32 = 0;    // use a 16 bit timer
+    OC1CONbits.OCFLT = 0;
+    OC1CONbits.OCTSEL = 1;  // use timer 3 (1 = 3, 0 = 2)
+    OC1CONbits.OCM = 6;     // set mode
+
+    IEC0bits.OC1IE = 1;
+    IPC1bits.OC1IP = 1;
+    IPC1bits.OC1IS = 1;
+
+    T3CONbits.TCKPS = 6;
+    T3CONbits.ON = 1;
+    T3CONbits.TON = 1;
+
+        while (1) {
+        WDTCONbits.WDTCLR = 1;
+    }
+}
+
 int main(void)
 {
-    tp3();
+    tp4();
     return (0);
 }
